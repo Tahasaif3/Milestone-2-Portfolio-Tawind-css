586073480482f1aba48a17ab67bfdd69672eb0cb
Taha Saif <tahasaifsurhio454@gmail.com>
2024-11-11 17:05:40 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
   /* config options here */
->>>>>>> 3e06035 (Initial commit from Create Next App)
-=======
->>>>>>> b4eaff8 (Resole merged conflicts)
 };
 
 export default nextConfig;
